99a015883527fe9f9eadf06b68bc2ecad0cf5a2f
aiot1025 <93106509+Aiot1025@users.noreply.github.com>
2021-10-26 09:29:21 +0800

    0929

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello World");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+
+const port = process.env.PORT || 80;
+
+app.get("/", (req, res) => {
+    const data = fs.readFileSync("./home/index.html", "utf8");
+    const bufferLen = Buffer.byteLength(data, "utf8");
+
+    let myHead = {
+        "Conntent-Type": "text/html; charset=UTF-8",
+        "Content-Length": bufferLen
+    };
+
+    res.writeHead(200, myHead);
+    res.write(data);
+    res.end();
+});
+
+
+app.listen(port);
