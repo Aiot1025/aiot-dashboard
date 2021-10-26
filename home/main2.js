315d6ec5cb1feb3dde3f7a352610b4d4a6a1d493
aiot1025 <93106509+Aiot1025@users.noreply.github.com>
2021-10-26 15:32:12 +0800

    0332

--- home/main2.js
+++ home/main2.js
@@ -1,7 +1,29 @@
-const nowTempData = document.getElementById("nowTempData");
-const nowHumData = document.getElementById("nowHumData");
+const nowDate       = document.getElementById("nowDate");
+const nowTime       = document.getElementById("nowTime");
+const nowTempData   = document.getElementById("nowTempData");
+const nowHumData    = document.getElementById("nowHumData");
+
+let nowDateYear     = 0;
+let nowDateMonth    = 0;
+let nowDateDate     = 0;
+let nowDateHours    = 0;
+let nowDateMinutes  = 0;
+let nowDateSeconds  = 0;
+
+function setNowDate() {
+    let date           = new Date();
+    nowDateYear        = date.getFullYear();
+    nowDateMonth       = date.getMonth() + 1;
+    nowDateDate        = date.getDate();
+    nowDateHours       = date.getHours();
+    nowDateMinutes     = date.getMinutes();
+    nowDateSeconds     = date.getSeconds();
+    nowDate.innerText = `${nowDateYear}年 ${nowDateMonth < 10 ? "0" + nowDateMonth : nowDateMonth}月 ${nowDateDate < 10 ? "0" + nowDateDate : nowDateDate}日`;
+    nowTime.innerText = `${nowDateHours < 10 ? "0" + nowDateHours : nowDateHours}:${nowDateMinutes < 10 ? "0" + nowDateMinutes : nowDateMinutes}:${nowDateSeconds < 10 ? "0" + nowDateSeconds : nowDateSeconds}`;
+}
 
 function setNowDHT11Data() {
+    console.log("setNowDHT11Data");
     let request = new XMLHttpRequest();
     request.open("POST", window.location.origin + "/data/getNowDHT11Data", true);
     request.setRequestHeader("Content-Type", "application/json");
@@ -11,15 +33,15 @@ function setNowDHT11Data() {
         let myJson = JSON.parse(request.responseText);
         console.log("Heroku Msg[Get Now DHT11 Data]:");
         console.log(myJson);
-
+        
         try {
-            let nowTime = new Date();
+            let nowTime  = new Date();
             let dataTime = new Date(myJson["Year"], myJson["Month"] - 1, myJson["Day"], myJson["Hour"], myJson["Min"]);
             let time = nowTime - dataTime;
             console.log("Get Now DHT11 Data[Time]:", time);
-            if(time > 180000){
+            if(time > 180000) {
                 nowTempData.innerText = "No Data";
-                nowHumData.innerText = "No Data";
+                nowHumData .innerText = "No Data";
             } else {
                 nowTempData.innerText = `${myJson["Temp"]}\u00B0C`;
                 nowHumData .innerText = `${myJson["Hum"]}%`;
@@ -27,5 +49,6 @@ function setNowDHT11Data() {
         } catch(e) {
             console.log("Get Now DHT11 Data[Error]:", e);
         }
+        
     });
 }
